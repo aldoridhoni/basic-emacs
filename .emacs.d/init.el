fb5842fab5ac8e489bb789252cc074a542c26f75
Aldo Ridhoni <aldoridhoni@gmail.com>
2019-05-15 16:30:53 +0800

    Add tangle filename.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,13 @@
 (setq custom-file (make-temp-file "emacs-custom"))
 (load custom-file)
 
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+;; Call python script to tangle this faster
+;; next line not work because HOME is probably not real.
+;; (add-to-list 'exec-path "~/bin")
+(let ((config-file (expand-file-name "config.org" user-emacs-directory)))
+  (with-demoted-errors "%S"
+    (call-process "org-babel-tangle" config-file nil nil))
+  (org-babel-load-file config-file))
 
 ;; EXWM
 (let ((config-exwm (expand-file-name "config-exwm.el" user-emacs-directory)))
