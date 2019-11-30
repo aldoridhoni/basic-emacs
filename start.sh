73e588e79b536fdad783d5551ac44f525950b89c
Aldo Ridhoni <aldoridhoni@gmail.com>
2019-11-30 19:12:59 +0700

    Coba perbaiki startup script

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 # https://stackoverflow.com/questions/29832037/how-to-get-script-directory-in-posix-sh
 
-dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
+
+script=$(readlink -- "$0")
+script_dir=$(dirname -- "$script")
+dir=$(CDPATH= cd -- "$script_dir" && pwd -P)
 emacs_bin=$(which emacs)
+
+ln -s $HOME/.config $script_dir/.config
+ln -s $HOME/.local $script_dir/.local
+
 exec env HOME="$dir" REAL_HOME="$HOME" $emacs_bin --maximized "$@"
