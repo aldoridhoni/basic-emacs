956e4854a7849f798d02d60d88963f22ac83ebdf
Aldo Ridhoni <aldoridhoni@gmail.com>
2022-01-13 07:29:32 +0800

    Tangle when needed
    Modified-by: Aldo Ridhoni <aldoridhoni@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,9 @@
 ;;; init.el --- Emacs init.el
 ;; (package-initialize)
 
+;;; Commentary:
+;; Initialize
+
 ;;; Code:
 ;; Load before-init.el
 (when (file-exists-p (concat user-emacs-directory "before-init.el"))
@@ -15,15 +18,21 @@
 ;; Call python script to tangle this faster
 ;; next line not work because HOME is probably not real.
 ;; (add-to-list 'exec-path "~/bin")
-(let ((config-file (expand-file-name "config.org" user-emacs-directory)))
-  (let ((default-directory user-emacs-directory))
-    (message "call process org-babel-tangle")
-    (with-demoted-errors "%S"
-      (with-output-to-string
-        (with-current-buffer
-            standard-output
-          (call-process "org-babel-tangle" config-file t t)))))
-  (org-babel-load-file config-file))
+(let ((config-file (expand-file-name "config.org" user-emacs-directory))
+      (config-lisp (expand-file-name "config.el" user-emacs-directory)))
+  (when (not (file-exists-p config-lisp))
+    (org-babel-load-file config-file))
+  (if (file-newer-than-file-p config-file config-lisp)
+      (progn
+        (let ((default-directory user-emacs-directory))
+          (message "call process org-babel-tangle")
+          (with-demoted-errors "%S"
+            (with-output-to-string
+              (with-current-buffer
+                  standard-output
+                (call-process "org-babel-tangle" config-file t t)))))
+        (org-babel-load-file config-file))
+    (load config-lisp)))
 
 ;; Private per machine config.el
 (let ((local-config (expand-file-name "config.el"
